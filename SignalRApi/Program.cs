6a519d36abf88fa50bf26e9aed0aa4f646cbdb8e
Burak Aslan <20360859045@ogrenci.btu.edu.tr>
2025-08-30 12:21:43 +0300

    Program.cs AddScoped işlemleri yapıldı

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,3 +1,8 @@
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAboutService,AboutManager>();
+builder.Services.AddScoped<IAboutDal,EfAboutDal>();
+
+builder.Services.AddScoped<IBookingService,BookingManager>();
+builder.Services.AddScoped<IBookingDal,EfBookingDal>();
+
+builder.Services.AddScoped<ICategoryService,CategoryManager>();
+builder.Services.AddScoped<ICategoryDal,EfCategoryDal>();
+
+builder.Services.AddScoped<IContactService,ContactManager>();
+builder.Services.AddScoped<IContactDal,EfContactDal>();
+
+builder.Services.AddScoped<IDiscountService,DiscountManager>();
+builder.Services.AddScoped<IDiscountDal,EfDiscountDal>();
+
+builder.Services.AddScoped<IFeatureService,FeatureManager>();
+builder.Services.AddScoped<IFeatureDal,EfFeatureDal>();
+
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+
+builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
+builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+builder.Services.AddScoped<ITestimonialDal,EfTestimonialDal>();
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 

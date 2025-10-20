e382447df0f34900537fde420af6e2eb6cc8c768
Burak Aslan <burakaslan2253@gmail.com>
2025-10-20 15:10:11 +0300

    Slider entity was added, Slider CRUD operations were completed.

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -65,6 +65,8 @@ builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
 builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
 builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());

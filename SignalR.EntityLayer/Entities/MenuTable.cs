57e9647346873f54aaca833eadaa3656adc6af1e
Burak Aslan <burakaslan2253@gmail.com>
2025-10-21 17:53:03 +0300

    Basket table added

--- SignalR.EntityLayer/Entities/MenuTable.cs
+++ SignalR.EntityLayer/Entities/MenuTable.cs
@@ -11,7 +11,8 @@ namespace SignalR.EntityLayer.Entities
 		public int MenuTableId { get; set; }
 		public string Name { get; set; }
 		public bool Status { get; set; }
+        public List<Basket> Baskets { get; set; }
 
 
-	}
+    }
 }
